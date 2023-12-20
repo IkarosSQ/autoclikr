c5c50b2a14b771d70bafb8a2674c4e04fd0704e6
IkarosSQ <federleyj@gmail.com>
2023-12-20 13:05:27 +0100

    Remove unnecessary tkinter code and fix start button command

--- clicker.py
+++ clicker.py
@@ -1,15 +1,9 @@
-import tkinter as tk
 from random import random
 from threading import Thread
 from time import sleep
 from pynput.mouse import Controller, Button
 from pynput.keyboard import KeyCode, Listener
 
-root = tk.Tk()
-root.geometry("445x370")
-root.background = "black"
-root.title("Ikaros' autoClicker")
-root.resizable(False, False)
 
 delay = 0.0001
 mouse = Controller()
@@ -27,10 +21,7 @@ def keypress(key):
     if key == KeyCode(char='-'):
         AutoClicker.clicking = not AutoClicker.clicking
 
-start_button = tk.Button(root, text="Start ('-')", command=AutoClicker().start())
-start_button.pack()
-
-root.mainloop()
+AutoClicker().start()
 
 with Listener(on_press=keypress) as listener:
     listener.join() 
