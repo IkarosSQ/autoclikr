2ba47e568f33f880e2fbe018578a67c79f72e64d
IkarosSQ <federleyj@gmail.com>
2024-01-19 13:13:35 +0100

    Update GUI and add button to toggle clicking

--- clicker.py
+++ clicker.py
@@ -1,18 +1,17 @@
+import tkinter as tk
 from random import random
 from threading import Thread
 from time import sleep
 from pynput.mouse import Controller, Button
 from pynput.keyboard import KeyCode, Listener
-import tkinter as tk
 
 root = tk.Tk()
 root.title("AutoClickr")
-root.geometry("180x40")
+root.geometry("300x100")
 root.resizable(False, False)
-label = tk.Label(root, text="Press - to start/stop clicking")
+root.attributes("-topmost", 1)
+label = tk.Label(root, text="Press the button or - to start/stop clicking", font=("Arial Bold", 10))
 label.pack()
-
-root.mainloop()
 
 delay = 0.00001
 mouse = Controller()
@@ -26,11 +25,21 @@ class AutoClicker(Thread):
                 mouse.click(Button.left, 1)
             sleep(delay * random() + 1/2 * delay)
 
+
 def keypress(key):
     if key == KeyCode(char='-'):
         AutoClicker.clicking = not AutoClicker.clicking
 
+def toggle_clicking():
+    AutoClicker.clicking = not AutoClicker.clicking
+
+
+click_button = tk.Button(root, text="Toggle Clicking", command=toggle_clicking)
+click_button.pack()
+
 AutoClicker().start()
 
-with Listener(on_press=keypress) as listener:
-    listener.join() 
+listener = Listener(on_press=keypress)
+listener.start()
+
+root.mainloop()
