01f9269d146ef5142a0cb22644b495c58e9cae82
IkarosSQ <federleyj@gmail.com>
2024-02-12 13:22:46 +0100

    Update clicker.py with new button functionality

--- clicker.py
+++ clicker.py
@@ -10,6 +10,8 @@ root.title("AutoClickr")
 root.geometry("300x100")
 root.resizable(False, False)
 root.attributes("-topmost", 1)
+root.background = "#34495E"
+root.foreground = "#D5D8DC"
 label = tk.Label(root, text="Press the button or - to start/stop clicking", font=("Arial Bold", 10))
 label.pack()
 
@@ -25,21 +27,43 @@ class AutoClicker(Thread):
                 mouse.click(Button.left, 1)
             sleep(delay * random() + 1/2 * delay)
 
+auto_clicker = AutoClicker()
 
 def keypress(key):
-    if key == KeyCode(char='-'):
-        AutoClicker.clicking = not AutoClicker.clicking
+    if key == KeyCode(char=''):
+        updatebuttons()
+        auto_clicker.clicking = not auto_clicker.clicking
 
 def toggle_clicking():
-    AutoClicker.clicking = not AutoClicker.clicking
+    updatebuttons()
+    print(auto_clicker.clicking, "text")
+    auto_clicker.clicking = not auto_clicker.clicking
 
+def updatebuttons():
+    if click_button["state"] == "normal":
+        click_button["state"] = "disabled"
+        click_button["text"] = "enable(f6)"
+        click_button1["state"] = "normal"
+        click_button1["text"] = "disable(f6)"
+        
+    else:
+        click_button1["state"] = "disabled"
+        click_button1["text"] = "disable(f6)"
+        click_button["state"] = "normal"
+        click_button["text"] = "enable(f6)"
+        
+    
 
-click_button = tk.Button(root, text="start Clicking", command=toggle_clicking)
-click_button.pack()
+click_button = tk.Button(root, text="enable(f6)", background = "#273746", foreground = "#D5D8DC", command=toggle_clicking)
+click_button1 = tk.Button(root, text="disable(f6)", background = "#273746", foreground = "#D5D8DC",  command=toggle_clicking, state = "disabled")
+click_button.pack(side = "left", expand = True)
+click_button1.pack(side = "right", expand = True)
 
-AutoClicker().start()
+auto_clicker.start()
 
 listener = Listener(on_press=keypress)
 listener.start()
+
+listener.stop()
 
 root.mainloop()
